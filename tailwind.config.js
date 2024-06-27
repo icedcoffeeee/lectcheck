cfbe5bdb7b11ee537ddc28f65242b15ada0ef7e7
icedcoffeeee <hazim.saharuddin@gmail.com>
2024-06-27 14:27:46 +0800

    lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,22 @@ import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,ts,svelte}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          primary: "#1d4ed8",
-          secondary: "#93c5fd",
-          accent: "#6d28d9",
-          neutral: "#18181b",
-          "base-100": "#dbeafe",
-        }
-      },
-    ],
-  }
-}
-
+	content: ['./src/**/*.{html,ts,svelte}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [daisyui],
+	daisyui: {
+		themes: [
+			{
+				light: {
+					primary: '#1d4ed8',
+					secondary: '#93c5fd',
+					accent: '#6d28d9',
+					neutral: '#18181b',
+					'base-100': '#dbeafe'
+				}
+			}
+		]
+	}
+};

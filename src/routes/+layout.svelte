78170b81ec01a5d7120c97fc4ae863296715a4bf
icedcoffeeee <hazim.saharuddin@gmail.com>
2024-07-13 01:46:46 +0800

    basic view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
 	import '../app.css';
-
 	import { injectSpeedInsights } from '@vercel/speed-insights';
+	import { onNavigate } from '$app/navigation';
+
 	injectSpeedInsights();
+	onNavigate(function (nav) {
+		///@ts-ignore
+		if (!document.startViewTransition) return;
+		return new Promise((res) =>
+			///@ts-ignore
+			document.startViewTransition(async function () {
+				res();
+				await nav.complete;
+			})
+		);
+	});
 </script>
 
 <slot />

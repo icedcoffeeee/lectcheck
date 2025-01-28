f0459ca55f82da5efde14dd171b6ec11561debe8
icedcoffeeee <hazim.saharuddin@gmail.com>
2025-01-28 15:09:59 +0800

    updates from dev

--- src/components/RatingBoard.svelte
+++ src/components/RatingBoard.svelte
@@ -1,4 +1,15 @@
 <script lang="ts">
+	import { readable } from 'svelte/store';
+
+	export const runes_to_store = (cb: () => string[]) =>
+		readable(cb(), (set) =>
+			$effect.root(() => {
+				$effect.pre(() => {
+					set(cb());
+				});
+			})
+		);
+
 	interface Props {
 		ratings: number[];
 		shortCategory?: boolean;
@@ -7,14 +18,14 @@
 
 	let { ratings, shortCategory = false, additionalClass = '' }: Props = $props();
 
-	let categories = $state(['Teaching', 'Assessment', 'Guidance', 'Reach']);
-	if (shortCategory) categories = categories.map((c) => c[0]);
+	let categories = runes_to_store(() => ['Teaching', 'Assessment', 'Guidance', 'Reach']);
+	if (shortCategory) categories = runes_to_store(() => $categories.map((c) => c[0]));
 </script>
 
 <div class="max-w-96 min-w-72 text-xs flex flex-col {additionalClass}">
 	{#each ratings as rating, i}
 		<div class="flex items-center gap-2">
-			<span class={`${shortCategory ? 'w-[3%]' : 'w-[30%] md:w-[33%]'}`}>{categories[i]}</span>
+			<span class={`${shortCategory ? 'w-[3%]' : 'w-[30%] md:w-[33%]'}`}>{$categories[i]}</span>
 			<progress class="progress progress-primary w-[100%]" value={rating} max="5"></progress>
 			<span class="w-[6%] text-right">{rating.toPrecision(2)}</span>
 		</div>

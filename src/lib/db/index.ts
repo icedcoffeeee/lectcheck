8e52fbfb4b53ffd2a6468eb612ef765c20a319e1
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-03-25 22:49:02 +0800

    better-auth setup

---
+++ src/lib/db/index.ts
@@ -0,0 +1,18 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
+import { DATABASE_URL } from '$env/static/private';
+
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = postgres(DATABASE_URL);
+
+export const db = drizzle(client, { schema });
+
+export type User = typeof schema.user.$inferSelect;
+export type Lect = typeof schema.lect.$inferSelect;
+export type Post = typeof schema.post.$inferSelect;
+export type Comment = typeof schema.comment.$inferSelect;
+
+export type LectI = typeof schema.lect.$inferInsert;
+export type PostI = typeof schema.post.$inferInsert;
+export type CommentI = typeof schema.comment.$inferInsert;
---
+++ src/lib/db/schema.ts
@@ -0,0 +1,46 @@
+import { pgTable as table } from 'drizzle-orm/pg-core';
+import { serial, text, varchar, integer } from 'drizzle-orm/pg-core';
+import { user } from './auth-schema';
+
+export * from './auth-schema';
+
+export const lect = table('lect', {
+	id: serial('id').primaryKey(),
+	tag: varchar('tag').notNull(),
+	name: text('name').notNull(),
+	image: text('image').notNull()
+});
+
+export const post = table('post', {
+	id: serial('id').primaryKey(),
+	text: text('text').notNull(),
+	class: varchar('class').notNull(),
+	rating: integer('rating').array().notNull(),
+	liked: text('liked')
+		.references(() => user.id)
+		.array()
+		.notNull(),
+	disliked: text('liked')
+		.references(() => user.id)
+		.array()
+		.notNull(),
+
+	user: text('user')
+		.notNull()
+		.references(() => user.id),
+	lect: integer('lect')
+		.notNull()
+		.references(() => lect.id)
+});
+
+export const comment = table('comment', {
+	id: serial('id').primaryKey(),
+	text: text('text').notNull(),
+
+	user: text('user')
+		.notNull()
+		.references(() => user.id),
+	post: integer('post')
+		.notNull()
+		.references(() => post.id)
+});

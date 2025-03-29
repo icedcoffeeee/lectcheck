9e9d56767ec7cfddfc9cab2e9a79c6b508a3fd7d
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-03-29 17:41:16 +0800

    prefer dark

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,8 +8,13 @@
   let { children, data } = $props();
 </script>
 
+<svelte:head>
+  <title>LectCheck</title>
+  <meta name="description" content="Your Lecturers At A Glance." />
+</svelte:head>
+
 <nav
-  class="sticky top-0 z-10 flex w-full justify-between p-5 font-mono text-sm font-bold backdrop-blur-md"
+  class="sticky top-0 z-10 flex w-full justify-between p-5 text-sm font-bold backdrop-blur-md"
 >
   <a href="/">LectCheck</a>
   <div class="flex gap-5">
@@ -25,3 +30,11 @@
 <div class="h-[calc(100svh-12*var(--spacing)-var(--text-sm))] p-5">
   {@render children()}
 </div>
+
+<style lang="postcss">
+  @reference "../app.css";
+
+  a {
+    @apply hover:text-primary;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,23 @@
   import { enhance } from "$app/forms";
 </script>
 
-<div class="flex h-full w-full justify-center">
-  <section class="flex h-full w-full max-w-lg flex-col justify-center">
+<div class="flex h-full w-full items-center justify-center">
+  <section class="flex h-fit w-full max-w-2xl flex-col justify-center gap-10 p-4">
     <div class="flex flex-col">
       <h1>LectCheck</h1>
-      <p>Your first stop for top instructors.</p>
+      <p>Your first stop for great education.</p>
     </div>
     <span class="h-10"></span>
-    <form method="post" use:enhance class="flex w-min flex-col gap-2 self-end">
-      <p class="flex justify-between">
-        <span> Enter lecturer tag.</span>
-        <button class="text-primary border-primary self-end">GO</button>
+    <form method="post" use:enhance class="flex flex-col gap-2 self-end">
+      <p class="flex justify-between gap-4">
+        <span>Enter lecturer tag.</span>
+        <button class="bg-primary border-primary self-end rounded px-2 text-white">GO</button>
       </p>
-      <input name="tag" placeholder="e.g. hamdi" class="border-y border-black px-2 font-mono" />
+      <input
+        name="tag"
+        placeholder="e.g. hamdi"
+        class="text-primary placeholder:text-foreground/40 border-foreground border-y px-2 py-1"
+      />
     </form>
   </section>
 </div>

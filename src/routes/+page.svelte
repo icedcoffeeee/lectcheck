4ed75b75383887ba9cfd7f2eb5259932f92899b9
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-03-30 00:24:15 +0800

    loader

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,40 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
+  import { Loader2 } from "lucide-svelte";
+
+  let loading = false;
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
   <section class="flex h-fit w-full max-w-2xl flex-col justify-center gap-10 p-4">
     <div class="flex flex-col">
       <h1>LectCheck</h1>
-      <p>Your first stop for great education.</p>
+      <p>Your first stop for better education.</p>
     </div>
     <span class="h-10"></span>
-    <form method="post" use:enhance class="flex flex-col gap-2 self-end">
+    <form
+      method="post"
+      use:enhance={() => {
+        loading = true;
+        return async ({ update }) => {
+          await update();
+        };
+      }}
+      class="flex flex-col gap-2 self-end"
+    >
       <p class="flex justify-between gap-4">
         <span>Enter lecturer tag.</span>
-        <button class="bg-primary border-primary self-end rounded px-2 text-white">GO</button>
+        <button
+          class="bg-primary border-primary flex w-10 justify-center self-end rounded text-white"
+        >
+          {#if loading}
+            <Loader2 class="text-baseline scale-50 animate-spin" color="white"></Loader2>
+          {:else}
+            GO
+          {/if}
+        </button>
       </p>
-      <input
-        name="tag"
-        placeholder="e.g. hamdi"
-        class="text-primary placeholder:text-foreground/40 border-foreground border-y px-2 py-1"
-      />
+      <input name="tag" placeholder="e.g. hamdi" class="border-foreground border-y px-2 py-1" />
     </form>
   </section>
 </div>
